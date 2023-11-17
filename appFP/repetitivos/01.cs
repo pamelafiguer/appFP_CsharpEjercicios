d7029c4c29d716f56e7ad4da87c1b91dfdb72947
= <=>
2023-11-16 22:53:12 -0500

    visual

---
+++ appFP/repetitivos/01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appFP.Repetitivos
+{
+    public partial class _01 : Form
+    {
+        public _01()
+        {
+            InitializeComponent();
+        }
+    }
+}
